bfcc2ad5a64b3b992dcf7233dafcd911eac29ead
Jaap <jaap@getstream.io>
2020-09-29 15:19:52 +0200

    unreadcount --no-verify

--- src/components/UnreadCount/UnreadCount.js
+++ src/components/UnreadCount/UnreadCount.js
@@ -2,28 +2,39 @@ import React, { useContext, useState, useEffect } from 'react';
 import { ChatContext } from '../../context';
 
 const UnreadCount = () => {
-  const { client } = useContext(ChatContext);
+  const { client, channel } = useContext(ChatContext);
   const [totalUnreadCount, setTotalUnreadCount] = useState(0);
-
+  
   const handleEvent = (e) => {
     if (e?.total_unread_count >= 0) {
+      if(totalUnreadCount === 0 && e.cid === channel?.cid) return;
       setTotalUnreadCount(e.total_unread_count);
     }
   };
 
   useEffect(() => {
     if (!client) return;
-
     setTotalUnreadCount(client.user.total_unread_count);
     client.on(handleEvent);
-  }, [client]);
+    return () => {
+      client.off(handleEvent);
+    }
+  }, [client, channel]);
 
   return (
     <div
       style={{
         position: 'absolute',
-        background: 'green',
-        top: 30,
+        background: 'magenta',
+        color: 'white',
+        fontWeight: 'bold',
+        width: 20,
+        height: 20,
+        borderRadius: 100,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 40,
         right: 30,
         padding: 10,
         zIndex: 100,

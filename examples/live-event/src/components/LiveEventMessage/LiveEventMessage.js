a7ced6f417f5fa9280fd7e776b0c099c15e1839f
Grant Steinke <grantjsteinke@gmail.com>
2020-11-09 14:06:24 -0700

    adds message component and actions

--- examples/live-event/src/components/LiveEventMessage/LiveEventMessage.js
+++ examples/live-event/src/components/LiveEventMessage/LiveEventMessage.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { MessageLivestream } from 'stream-chat-react';
+import { PinIcon } from '../../assets/PinIcon';
+import { LiveEventReactions } from '../LiveEventReactions/LiveEventReactions';
 
 import './LiveEventMessage.css';
 
 export const LiveEventMessage = (props) => {
+  const { message, setPinnedMessages } = props;
+  const [isPinned, setIsPinned] = useState(false);
+
   return (
     <div>
-      <MessageLivestream {...props} ReactionSelector={() => null} />
+      <div
+        style={{ height: '20px', width: '20px', background: 'red' }}
+        onClick={() => {
+          setPinnedMessages((prevMessages) => {
+            if (!isPinned) {
+              setIsPinned(true);
+              return {
+                ...prevMessages,
+                [message.id]: message,
+              };
+            }
+            setIsPinned(false);
+            const copy = prevMessages;
+            delete copy[message.id];
+            return copy;
+          });
+        }}
+      >
+        {isPinned && <PinIcon />}
+      </div>
+      <MessageLivestream {...props} ReactionsList={LiveEventReactions} />
     </div>
   );
 };

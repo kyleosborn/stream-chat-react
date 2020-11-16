3aaafe1633c035c66de976a64a9ec48dd243be30
Grant Steinke <grantjsteinke@gmail.com>
2020-11-16 13:15:38 -0700

    code cleanup

--- examples/live-event/src/components/LiveEventAttendees/LiveEventAttendees.js
+++ examples/live-event/src/components/LiveEventAttendees/LiveEventAttendees.js
@@ -6,14 +6,6 @@ import './LiveEventAttendees.css';
 export const LiveEventAttendees = () => {
   return (
     <div className="live-event-attendees__container">
-      {/* <div className="live-event-attendees__top">
-        <div className="live-event-attendees__top-title">Moderators</div>
-        <div className="live-event-attendees__top-pictures">
-          <div>{Mod1}</div>
-          <div>{Mod1}</div>
-        </div>
-      </div>
-      <div className="live-event-attendees__bottom"></div> */}
       <ModerationSidebar />
     </div>
   );

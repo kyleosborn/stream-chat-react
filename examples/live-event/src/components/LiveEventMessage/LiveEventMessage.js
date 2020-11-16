244bf9fa97c7386dd91f85da516e2fb2123519a9
Grant Steinke <grantjsteinke@gmail.com>
2020-11-16 13:24:04 -0700

    adds ability to pin messages

--- examples/live-event/src/components/LiveEventMessage/LiveEventMessage.js
+++ examples/live-event/src/components/LiveEventMessage/LiveEventMessage.js
@@ -1,36 +1,55 @@
 import React, { useState } from 'react';
 
-import { MessageLivestream } from 'stream-chat-react';
+import { MessageLivestream, MessageActions } from 'stream-chat-react';
 import { PinIcon } from '../../assets/PinIcon';
 import { LiveEventReactions } from '../LiveEventReactions/LiveEventReactions';
 
 import './LiveEventMessage.css';
 
 export const LiveEventMessage = (props) => {
-  const { message, setPinnedMessages } = props;
-  const [isPinned, setIsPinned] = useState(false);
+  const { message, pinnedMessagesIds, setPinnedMessages } = props;
+  const isMessagePinned = pinnedMessagesIds?.find((id) => id === message.id);
+  const [isPinned, setIsPinned] = useState(isMessagePinned);
+
+  const pinChecker = () => {
+    setPinnedMessages((prevMessages) => {
+      if (!isPinned) {
+        setIsPinned(true);
+        return {
+          ...prevMessages,
+          [message.id]: message,
+        };
+      }
+      setIsPinned(false);
+      const copy = prevMessages;
+      delete copy[message.id];
+      return copy;
+    });
+  };
+
+  const getMessageActions = () => ['edit', 'delete', 'react', 'reply', 'mute'];
 
   return (
-    <div>
+    <div
+      className={
+        isPinned
+          ? 'live-event-message__container__pinned'
+          : 'live-event-message__container__unpinned'
+      }
+    >
       <div
-        style={{ height: '20px', width: '20px', background: 'red' }}
-        onClick={() => {
-          setPinnedMessages((prevMessages) => {
-            if (!isPinned) {
-              setIsPinned(true);
-              return {
-                ...prevMessages,
-                [message.id]: message,
-              };
-            }
-            setIsPinned(false);
-            const copy = prevMessages;
-            delete copy[message.id];
-            return copy;
-          });
-        }}
+        className="pin-icon"
+        style={{ marginLeft: '15px' }}
+        onClick={pinChecker}
       >
-        {isPinned && <PinIcon />}
+        {isMessagePinned || isPinned ? <PinIcon /> : null}
+      </div>
+      <div className="new-actions">
+        <MessageActions
+          {...props}
+          handleMute={pinChecker}
+          getMessageActions={getMessageActions}
+        />
       </div>
       <MessageLivestream {...props} ReactionsList={LiveEventReactions} />
     </div>

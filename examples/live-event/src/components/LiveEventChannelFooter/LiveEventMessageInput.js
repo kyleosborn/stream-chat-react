9d1e5f4dd1ce54371afecd71db4caa37592f3ed3
Grant Steinke <grantjsteinke@gmail.com>
2020-11-16 13:21:28 -0700

    adds message input

---
+++ examples/live-event/src/components/LiveEventChannelFooter/LiveEventMessageInput.js
@@ -0,0 +1,64 @@
+import React, { useContext, useRef, useState } from 'react';
+import {
+  ChannelContext,
+  ChatAutoComplete,
+  EmojiPicker,
+  useMessageInput,
+} from 'stream-chat-react';
+import { SmileyFace } from '../../assets/SmileyFace';
+import { PaperClip } from '../../assets/PaperClip';
+
+import './LiveEventMessageInput.css';
+
+export const LiveEventMessageInput = (props) => {
+  const { sendMessage, thread } = useContext(ChannelContext);
+  const messageInput = useMessageInput({
+    ...props,
+    sendMessage,
+    parent: thread,
+  });
+  const [canSend, setCanSend] = useState(true);
+
+  const canSendTimer = useRef(null);
+
+  const selectEmoji = (emoji) => {
+    messageInput.onSelectEmoji(emoji);
+  };
+
+  const handleSubmitDelayed = (e) => {
+    if (canSend) {
+      messageInput.handleSubmit(e);
+      setCanSend(false);
+      canSendTimer.current = setTimeout(
+        () => setCanSend(true),
+        props.sloMoDelay * 1000,
+      );
+    } else {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div className="live-event-message-input__wrapper">
+      <div className="live-event-message-input__input">
+        <EmojiPicker {...messageInput} onSelectEmoji={selectEmoji} />
+
+        <ChatAutoComplete
+          innerRef={messageInput.textareaRef}
+          handleSubmit={handleSubmitDelayed}
+          onChange={messageInput.handleChange}
+          onSelectItem={messageInput.onSelectItem}
+          value={messageInput.text}
+          placeholder={'Send a message'}
+          onPaste={messageInput.onPaste}
+        />
+      </div>
+      <div className="live-event-message-input__input-buttons">
+        <PaperClip />
+        <div onClick={messageInput.openEmojiPicker}>
+          <SmileyFace />
+        </div>
+      </div>
+    </div>
+  );
+};

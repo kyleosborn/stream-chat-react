bf4cf8d53016e7cb9ae186040bd6ccf8c431aa92
Grant Steinke <grantjsteinke@gmail.com>
2020-11-16 13:10:35 -0700

    adds  pinned button and video

--- examples/live-event/src/App.js
+++ examples/live-event/src/App.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
 import React from 'react';
 import { StreamChat } from 'stream-chat';
-import { Chat, Channel, Window, Thread } from 'stream-chat-react';
+import {
+  Chat,
+  Channel,
+  Window,
+  Thread,
+  Streami18n,
+  enTranslations,
+} from 'stream-chat-react';
 import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 import { LiveEventChannelFooter } from './components/LiveEventChannelFooter/LiveEventChannelFooter';
-import { LiveEventChannelHeader } from './components/LiveEventChannelHeader/LiveEventChannelHeader';
+import { LiveEventChannelSwitch } from './components/LiveEventChannelSwitch/LiveEventChannelSwitch';
 import { LiveEventMessage } from './components/LiveEventMessage/LiveEventMessage';
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -17,9 +23,18 @@ const userToken =
   urlParams.get('user_token') ||
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZXhhbXBsZS11c2VyIn0.HlC0dMKL43y3K_XbfvQS_Yc3V314HU4Z7LrBLil777g';
 
+const i18nInstance = new Streami18n({
+  language: 'en',
+  translationsForLanguage: {
+    ...enTranslations,
+    Mute: 'Pin Message',
+    Unmute: 'Unpin Message',
+  },
+});
+
 const App = () => {
   const chatClient = new StreamChat(apiKey);
-  chatClient.setUser({ id: user, role: 'moderator' }, userToken);
+  chatClient.setUser({ id: user }, userToken);
   const channel = chatClient.channel('livestream', channelName, {
     image:
       'https://images.unsplash.com/photo-1512138664757-360e0aad5132?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80',
@@ -40,20 +55,48 @@ const App = () => {
         borderRadius: '16px',
       }}
     >
-      <Chat client={chatClient} theme={`livestream ${theme}`}>
+      <Chat
+        client={chatClient}
+        i18nInstance={i18nInstance}
+        theme={`livestream ${theme}`}
+      >
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div style={{ height: '800px', width: '810px' }}>
+            <div
+              style={{
+                height: '800px',
+                width: '810px',
+                borderRadius: '16px 0px 0px 16px',
+                overflow: 'hidden',
+              }}
+            >
+              <iframe
+                width="810px"
+                height="473px"
+                src="https://www.youtube.com/embed/_J4QPz52Sfo"
+                frameBorder="0px"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
               <img
-                style={{ height: '800px', width: '810px' }}
-                src={require('./assets/sampleVideo.png')}
+                style={{ height: '295px', width: '810px' }}
+                // eslint-disable-next-line global-require
+                src={require('./assets/VideoFooter.png')}
               ></img>
             </div>
           </div>
-          <div style={{ height: '800px', width: '320px' }}>
+          <div
+            style={{
+              height: '800px',
+              width: '320px',
+              boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.2)',
+              borderRadius: '0px 16px 16px 0px',
+              overflow: 'hidden',
+            }}
+          >
             <Channel channel={channel}>
               <Window hideOnThread>
-                <LiveEventChannelHeader />
+                <LiveEventChannelSwitch />
               </Window>
               <Thread
                 additionalMessageListProps={{ TypingIndicator: () => null }}

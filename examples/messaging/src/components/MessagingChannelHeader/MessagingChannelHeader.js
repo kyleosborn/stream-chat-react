33744cc1aac61e4c0d3ba39ce33a65c8fb810c82
Dan Carbonell <dan@getstream.io>
2020-12-07 12:51:35 -0700

    add images to channel header

--- examples/messaging/src/components/MessagingChannelHeader/MessagingChannelHeader.js
+++ examples/messaging/src/components/MessagingChannelHeader/MessagingChannelHeader.js
@@ -5,32 +5,117 @@ import './MessagingChannelHeader.css';
 
 import { TypingIndicator } from '../TypingIndicator/TypingIndicator';
 
+const getAvatarGroup = (members) => {
+  if (members.length === 1) {
+    return <Avatar image={members[0]?.user.image || undefined} size={40} />;
+  }
+
+  if (members.length === 2) {
+    return (
+      <div className="messaging__channel-header__avatars two">
+        <span>
+          <Avatar
+            image={members[0]?.user.image || undefined}
+            shape="square"
+            size={40}
+          />
+        </span>
+        <span>
+          <Avatar
+            image={members[1]?.user.image || undefined}
+            shape="square"
+            size={40}
+          />
+        </span>
+      </div>
+    );
+  }
+
+  if (members.length === 3) {
+    return (
+      <div className="messaging__channel-header__avatars three">
+        <span>
+          <Avatar
+            image={members[0]?.user.image || undefined}
+            shape="square"
+            size={40}
+          />
+        </span>
+        <span>
+          <Avatar
+            image={members[1]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+          <Avatar
+            image={members[2]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+        </span>
+      </div>
+    );
+  }
+
+  if (members.length >= 4) {
+    return (
+      <div className="messaging__channel-header__avatars four">
+        <span>
+          <Avatar
+            image={members[0]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+          <Avatar
+            image={members[1]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+        </span>
+        <span>
+          <Avatar
+            image={members[2]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+          <Avatar
+            image={members[3]?.user.image || undefined}
+            shape="square"
+            size={20}
+          />
+        </span>
+      </div>
+    );
+  }
+
+  return null;
+};
+
 const MessagingChannelHeader = () => {
   const { channel, client } = useContext(ChannelContext);
 
   const [title, setTitle] = useState('');
 
+  const members = Object.values(channel.state?.members || {});
+  const otherMembers = members.filter(
+    (member) => member.user?.id !== client?.user?.id,
+  );
+
   useEffect(() => {
-    const currentUserId = client?.user?.id;
     const channelName = channel?.data?.name;
 
     if (channelName) return channelName;
-
-    const members = Object.values(channel.state?.members || {});
-    const otherMembers = members.filter(
-      (member) => member.user?.id !== currentUserId,
-    );
 
     return setTitle(
       otherMembers
         .map((member) => member.user?.name || member.user?.id || 'Unnamed User')
         .join(', '),
     );
-  }, [channel.data.name, channel.state.members, client.user.id]);
+  }, [channel, otherMembers]);
 
   return (
     <div className="messaging__channel-header">
-      <Avatar size={32} />
+      {getAvatarGroup(otherMembers)}
       <div className="messaging__channel-header__right">
         <div className="channel-header__name">{title}</div>
         <TypingIndicator />

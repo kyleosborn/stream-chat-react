bce5d89c70b4b8b38efcc8b5408574847ca9cb9a
Dan Carbonell <dan@getstream.io>
2020-12-07 12:03:56 -0700

    refine channel list

--- examples/messaging/src/components/MessagingChannelList/MessagingChannelList.js
+++ examples/messaging/src/components/MessagingChannelList/MessagingChannelList.js
@@ -1,51 +1,64 @@
 import React, { useContext } from 'react';
-import {
-  Avatar,
-  ChatContext,
-  ChatDown,
-  LoadingChannels,
-} from 'stream-chat-react';
+import { Avatar, ChatContext } from 'stream-chat-react';
 
 import './MessagingChannelList.css';
 
 import { CreateChannelIcon } from '../../assets';
 
 const MessagingChannelList = ({
+  children,
   error = false,
   loading,
-  LoadingErrorIndicator = ChatDown,
-  LoadingIndicator = LoadingChannels,
-  children,
   onCreateChannel,
 }) => {
   const { client } = useContext(ChatContext);
-  const { id, image, name } = client.user || {};
+  const {
+    id,
+    image = require('../../assets/stream.png'),
+    name = 'Example User',
+  } = client.user || {};
+
+  const ListHeaderWrapper = ({ children }) => {
+    return (
+      <div className="messaging__channel-list">
+        <div className="messaging__channel-list__header">
+          <Avatar image={image} name={name} size={40} />
+          <div className="messaging__channel-list__header__name">
+            {name || id}
+          </div>
+          <button
+            className="messaging__channel-list__header__button"
+            onClick={onCreateChannel}
+          >
+            <CreateChannelIcon />
+          </button>
+        </div>
+        {children}
+      </div>
+    );
+  };
 
   if (error) {
-    return <LoadingErrorIndicator type="Connection Error" />;
+    return (
+      <ListHeaderWrapper>
+        <div className="messaging__channel-list__message">
+          Error loading conversations, please try again momentarily.
+        </div>
+      </ListHeaderWrapper>
+    );
   }
 
   if (loading) {
-    return <LoadingIndicator />;
+    return (
+      <ListHeaderWrapper>
+        <div className="messaging__channel-list__message">
+          Loading conversations...
+        </div>
+      </ListHeaderWrapper>
+    );
   }
 
-  return (
-    <div className="messaging__channel-list">
-      <div className="messaging__channel-list__header">
-        <Avatar image={image} name={name || id} size={40} />
-        <div className="messaging__channel-list__header__name">
-          {name || id}
-        </div>
-        <button
-          className="messaging__channel-list__header__button"
-          onClick={onCreateChannel}
-        >
-          <CreateChannelIcon />
-        </button>
-      </div>
-      {children}
-    </div>
-  );
+  return <ListHeaderWrapper>{children}</ListHeaderWrapper>;
 };
 
 export default React.memo(MessagingChannelList);
